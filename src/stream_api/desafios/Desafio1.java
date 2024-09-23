3e929622b1531d2b6c226d791ed0a351e2b58c8a
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-09-23 19:48:37 +0000

    Desafio 1: Correção
    Retirando a redundancia adicionando uma lista em hash que limita os valores a somente um por vez
    
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

--- src/stream_api/desafios/Desafio1.java
+++ src/stream_api/desafios/Desafio1.java
@@ -1,8 +1,9 @@
 package stream_api.desafios;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Desafio1 {
     
@@ -14,7 +15,13 @@ public class Desafio1 {
         // Criação da lista
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        // 
-        numeros.stream().sorted().toList().forEach(System.out::println);
+        // Criar uma lista para armazenar os valores unicos da lista original
+        HashSet<Integer> numerosUnicos = new HashSet<>();
+ 
+        // Criação do stream para ordenar e imprmir os valores
+       numeros.stream().sorted()
+                .filter(numero -> numerosUnicos.add(numero)) // filtra os valores unicos
+                .toList() // transforma em lista
+                .forEach(System.out::println); //imprime a medida que parsear pelos valores
     }
 }
