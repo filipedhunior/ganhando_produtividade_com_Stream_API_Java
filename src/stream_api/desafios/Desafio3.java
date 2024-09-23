855528440d5984c4693dfb05d3ded81565cf82bd
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-09-23 20:36:57 +0000

    Desafio 3: Completo
    Aprendi a usar as funções filter e distinc.
    
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

---
+++ src/stream_api/desafios/Desafio3.java
@@ -0,0 +1,26 @@
+package stream_api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/*
+* Desafio 3 - Verifique se todos os números da lista são positivos:
+* Com a ajuda da Stream API, 
+* passo 1: verifique se todos os números da lista são positivos 
+* passo 2: e exiba o resultado no console. 
+*/
+
+public class Desafio3 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // passo 01: comparar com zero
+        // Definição de numero positivo: Qualquer número que, 
+        // ao ser comparado com zero, for maior que zero é positivo.
+        Predicate<Integer> ePositivo = numero -> numero > 0;
+
+        // passo 02: exibir o resultado no console
+        numeros.stream().filter(ePositivo).distinct().forEach(System.out::println);
+    }
+}
