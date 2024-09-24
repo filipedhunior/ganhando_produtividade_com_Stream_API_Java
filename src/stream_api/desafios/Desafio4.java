0eaa4ce6d69597959f09142090098fa5af422673
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-09-24 17:58:28 +0000

    Desafio 4: Concluido
    Usado List e stream para gerar uma nova lista com os valores filtrados que se aproveitassem da expressão lambda
    
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

---
+++ src/stream_api/desafios/Desafio4.java
@@ -0,0 +1,27 @@
+package stream_api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Desafio4 {
+    /* 
+     * Desafio 04:
+     * Utilize a Stream API para remover os valores ímpares da lista 
+     * e imprima a lista resultante no console.
+    */
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Remover valores impares
+        Predicate<Integer> removeImpares = numero -> numero %2==0;
+
+        // Declarar outra lista, criar uma stream que filtra os valores pares e salvar como uma nova lista o resultado
+        List<Integer> numerosPares = numeros.stream()
+            .filter(removeImpares) // usa a expressão lambda removeImpares para filtar os valores pares
+            .collect(Collectors.toList()); // usar a interface Collectors para salvar numa nova lista
+
+        numerosPares.forEach(System.out::println);
+    }
+}
