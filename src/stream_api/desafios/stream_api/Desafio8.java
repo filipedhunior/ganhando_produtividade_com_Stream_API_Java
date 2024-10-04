a7bd63f6240b125b3aca2e1d79dab203e4b60a2d
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-10-04 18:15:50 +0000

    Desafio 8: Revisar posteriormente
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

---
+++ src/stream_api/desafios/stream_api/Desafio8.java
@@ -0,0 +1,24 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio8 {
+    /* Desafio 8: Somar os dígitos de todos os números da lista
+     * Utilizando a Stream API, realize a soma dos dígitos 
+     * de todos os números da lista 
+     * e exiba o resultado no console.
+     */
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int somaDigitos = numeros.stream()
+        .flatMapToInt(num -> String.valueOf(num)
+            .chars()
+            .map(Character::getNumericValue))
+        .distinct()
+        .sum();
+
+    System.out.println("Soma dos dígitos: " + somaDigitos);
+}
+}
