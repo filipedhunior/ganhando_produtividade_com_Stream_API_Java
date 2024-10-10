4cac90a1da1094a48258aca8fdac99bb63dce737
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-10-10 17:30:57 +0000

    Desafio 14: Em dores de parto
    Depois de mais de uma semana sem mecher no Java, esqueci completamente como codar
    
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

---
+++ src/stream_api/desafios/Desafio14.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio14 {
+    /* DESAFIO 14 
+    * Encontre o maior numero primo da lista e exiba no console
+    */ 
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    numeros.stream()
